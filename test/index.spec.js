31b2e560c708b02c801ab78162f5a39f5e31da07
Jensen Bernard <jensenbernard5@gmail.com>
2017-02-25 16:40:15 +0100

    Add tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,18 @@
+import path from 'path';
+import fsExtra from 'fs-extra';
 import capture from '../src/index';
 
 describe('Default tests', () => {
-  it('should add', (done) => {
+  it('should request valid url', (done) => {
+    const file = path.resolve(__dirname, 'output.png');
+    capture('http://google.com', file)
+    .then(() => {
+      fsExtra.removeSync(file);
+      done();
+    })
+    .catch(done);
+  }).timeout(10000);
+  it('should not request invalid url', (done) => {
     capture('invalid', '')
     .then(() => { done('Should not succeed!'); })
     .catch(() => done());
