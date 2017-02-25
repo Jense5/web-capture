324c7610ce5af712495bedbfeeb7af9cb4a65f7c
Jensen Bernard <jensenbernard5@gmail.com>
2017-02-25 16:08:33 +0100

    Update cli

--- src/cli.js
+++ src/cli.js
@@ -6,20 +6,34 @@ import chalk from 'chalk';
 import winston from 'winston';
 import commander from 'commander';
 
-import math from './index';
+import capture from './index';
+
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, { showLevel: false });
 
 const pkg = path.resolve(__dirname, '../package.json');
 const conf = JSON.parse(fs.readFileSync(pkg, 'utf8'));
+winston.info(chalk.bold(`WEB-CAPTURE v${conf.version}`));
 
 commander
 .version(conf.version)
 .usage('<options>')
-.option('-s, --sample', 'Add sample')
-.option('-n, --number [number]', 'Sample number', parseInt)
+.option('-u, --url [url]', 'Url to capture')
+.option('-o, --output [output]', 'Output path')
 .parse(process.argv);
 
-const number = commander.number || 3;
+if (!commander.url) {
+  winston.error(chalk.red('No url provided. Please use the --url option.'));
+  process.exit();
+}
 
-winston.info(`${chalk.green('Hello world!')}`);
-if (commander.sample) { winston.info(`${chalk.blue('Sample: ')} true`); }
-winston.info(`${chalk.green('Result:')} ${math.add(1, number)}`);
+const rawOutputPath = commander.output || 'output.png';
+const outputPath = path.resolve(__dirname, rawOutputPath);
+const outputFile = path.extname(outputPath) === '.png' ? outputPath : `${outputPath}.png`;
+
+winston.info(`Capturing ${chalk.cyan(commander.url)}`);
+winston.info(`Writing to ${chalk.cyan(outputFile)}`);
+
+capture(commander.url, outputFile)
+.then(() => winston.info(chalk.cyan('Done.')))
+.catch(error => winston.error(chalk.red(error)));
